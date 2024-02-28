93d151d296ad9a510e9e9f8f5d99554722364e87
broIsCoder <dhungelsafal@gmail.com>
2024-02-28 21:13:12 +0545

    last3

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -7,8 +7,6 @@ const Protected = ({ children, authentication = true }) => {
     const authStatus = useSelector((state) => state.auth.status);
     const navigate = useNavigate();
 
-    const [loader, setLoader] = useState(false);
-
     useEffect(() => {
         // if page need authentication and user is not loggedin
         if (authentication && !authStatus) {
@@ -19,18 +17,11 @@ const Protected = ({ children, authentication = true }) => {
         }
     }, [authStatus, authentication, navigate]);
 
-    return loader ?
-        (
-            <Container className={'flex items-center justify-center'}>
-                <img src="/loading .gif" className='w-20 h-20' alt="loading" />
-            </Container>
-        )
-        :
-        (
-            <Container>
-                {children}
-            </Container>
-        )
+    return (
+        <Container>
+            {children}
+        </Container>
+    )
 }
 
 export default Protected
